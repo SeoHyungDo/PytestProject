b8638a2b8638b70de56b389ff0bc8058b6b9e23c
SeoHyungDo <tjg106@gmail.com>
2025-08-29 03:13:44 +0900

    0829 update

--- test_pytest.py
+++ test_pytest.py
@@ -1,12 +1,12 @@
 def test_firstprogram() :
     print("Hello")
 
-def test_secondprogram(conf) :
+def test_secondprogram() :
     a = 512312312321
     b = 312312123
 
     assert '9' in str(a)
 
 #크롬, 파이어폭스, IE 순차 실행
-def test_crossbrowser(BrowserCrosscheck) :
-    print(BrowserCrosscheck)
+def test_crossbrowser(browsercrosscheck) :
+    print(browsercrosscheck)
