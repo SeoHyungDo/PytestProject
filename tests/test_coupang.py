8709028100923d9948be17e9fd645babcb4a2067
SeoHyungDo <tjg106@gmail.com>
2025-09-02 03:20:19 +0900

    0901 update

--- tests/test_coupang.py
+++ tests/test_coupang.py
@@ -19,3 +19,10 @@ class Test_CoupangTest(passclass) :
         apply_store_text = apply_store.text
         assert apply_store_text == "입점신청"
 
+    def test_apply_store_selectbox(self):
+        expected_texts = "오픈마켓\n여행·티켓\n로켓배송\n제휴마케팅\n로켓그로스"
+        self.driver.find_element(By.XPATH, '//*[@id="subscribeHeader"]/li[2]/a').click()
+        apply_store_select_box = self.driver.find_elements(By.XPATH,'//*[@id="subscribeHeader"]/li[2]/p')
+        for select_box_text in apply_store_select_box:
+            apply_store_select_box_text = select_box_text.text
+        assert apply_store_select_box_text == expected_texts
--- youtube_notlogin.py
+++ youtube_notlogin.py
@@ -17,29 +17,15 @@ chrome_option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service_obj)
 action = ActionChains(driver) # ActionChains 선언
 
-driver.get("https://www.youtube.com")
+driver.get("https://www.coupang.com")
 chrome_option.add_argument("—ignore-certificate-errors") #인증 오류 무시
 driver.maximize_window()
 
-#브라우저의 타이틀을 받아옴
-browser_title = driver.title
-print("받아온 브라우저 타이틀 ==> " + browser_title)
-assert(browser_title == "YouTube"), "YouTube Title 불일치"
-
-#브라우저의 URL을 받아옴
-browser_Url = driver.current_url
-print("받아온 브라우저 URL ==> " + browser_Url)
-assert(browser_Url == "https://www.youtube.com/"), "유튜브 URL 불일치"
-
-try :
-    if driver.find_element(By.ID, "guide-button").is_displayed() :
-        print("guide-button 노출 확인!")
-
-except Exception as e :
-    print("Guide-button 미 노출")
-
-# dropdown = Select(driver.find_element(By,)) 드롭다운 테스트
-# selenium.webdriver.support.select.Select 임포트
+driver.find_element(By.XPATH, '//*[@id="subscribeHeader"]/li[2]/a').click()
+apply_store_select_box = driver.find_elements(By.XPATH,'//*[@id="subscribeHeader"]/li[2]')
+for select_box_text in apply_store_select_box :
+    apply_store_select_box_text = select_box_text.text
+    print("selectbox 내의 텍스트 목록 : " + apply_store_select_box_text)
 
 time.sleep(10)
 driver.close()
