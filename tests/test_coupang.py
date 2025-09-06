5180d84558fc958f8dd9d13c2269c37f4e9e5a63
SeoHyungDo <tjg106@gmail.com>
2025-09-06 17:46:23 +0900

    0904 update

--- tests/test_coupang.py
+++ tests/test_coupang.py
@@ -2,27 +2,26 @@ import pytest
 import time
 from selenium.webdriver.common.by import By
 from utility.passclass import passclass
-from vtest_CoupangTest import driver
-
+from pageobject.coupang_obj import coupang_main
 
 class Test_CoupangTest(passclass) :
 
     def test_favorite_name(self) :
-        favorite_name_obj = coupang_main(self.driver)
-        favorite_name = self.driver.find_element(By.XPATH,'//*[@id="wa-bookmark"]')
-        favorite_name_text = favorite_name.text
+        Coupang_main = coupang_main(self.driver)
+        favorite_name_text = Coupang_main.favorite_name_obj().text
         assert favorite_name_text == "즐겨찾기"
 
     def test_apply_store(self) :
-        apply_store = self.driver.find_element(By.XPATH,'//*[@id="subscribeHeader"]/li[2]/a')
-        apply_store_text = apply_store.text
+        Coupang_main = coupang_main(self.driver)
+        apply_store_text = Coupang_main.apply_store_obj().text
         assert apply_store_text == "입점신청"
 
-    def test_apply_store_selectbox(self):
+    def test_apply_store_selectbox_action(self):
+        Coupang_main = coupang_main(self.driver)
         expected_text = "오픈마켓\n여행·티켓\n로켓배송\n제휴마케팅\n로켓그로스"
-        self.driver.find_element(By.XPATH, '//*[@id="subscribeHeader"]/li[2]/a').click()
-        apply_store_select_box = self.driver.find_elements(By.XPATH,'//*[@id="subscribeHeader"]/li[2]/p')
-        for select_box_text in apply_store_select_box:
+        Coupang_main.apply_store_select_box_obj().click()
+        apply_store_select_box_data = self.driver.find_elements(By.XPATH,'//*[@id="subscribeHeader"]/li[2]/p')
+        for select_box_text in apply_store_select_box_data:
             apply_store_select_box_text = select_box_text.text
         assert apply_store_select_box_text == expected_text
 
